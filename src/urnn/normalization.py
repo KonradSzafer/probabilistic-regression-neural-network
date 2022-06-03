1cd0562143012493d58e9e523bc34a2ac4c376fe
KonradSzafer <szafer.konrad@gmail.com>
2022-06-04 01:49:24 +0200

    normalization update

--- src/urnn/normalization.py
+++ src/urnn/normalization.py
@@ -4,29 +4,65 @@ import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
 
+def MinMaxNormalizer(x):
+    y = (x - torch.min(x)) / (torch.max(x) - torch.min(x))
+    return y
+
+
+def ProbabilityNormalizer(x):
+    y = x / torch.sum(x)
+    return y
+
+
+def LinearlyNormalized(x):
+    y = MinMaxNormalizer(x)
+    y = ProbabilityNormalizer(y)
+    return y
+
+
 def Softmax(x):
     y = nn.Softmax(dim=-1)(x)
     return y
 
 
-def ExpSoftmax(x, base: float=1.0):
-    x = base * torch.exp(x)
+def ExpSoftmax(x, factor: float=1.0):
+    x = factor * torch.exp(x)
     y = nn.Softmax(dim=-1)(x)
     return y
 
 
+def Sigmoid(x):
+    y = nn.Sigmoid()(x)
+    y = ProbabilityNormalizer(y)
+    return y
+
+
 def ReLU(x):
-    y = F.normalize(x, dim=-1)
-    y = nn.ReLU()(y)
+    y = nn.ReLU()(x)
+    y = ProbabilityNormalizer(y)
+    return y
+
+
+def LeakyReLU(x, negative_slope=0.1):
+    y = nn.LeakyReLU(negative_slope)(x)
+    y = torch.add(y, -torch.min(y))
+    y = ProbabilityNormalizer(y)
     return y
 
 
 if __name__ == '__main__':
-    x = torch.linspace(-1.5, .95, steps=15)
+    # x = torch.linspace(-1.5, .95, steps=15)
+    # x = torch.linspace(-1.5, .95, steps=200)
+    x = torch.linspace(-3.5, 1.5, steps=30)
+    plt.plot(x, LinearlyNormalized(x), label='Linearly Normalized')
     plt.plot(x, Softmax(x), label='Softmax')
-    plt.plot(x, ExpSoftmax(x, base=1.0), label='Exp Softmax 1.0')
-    plt.plot(x, ExpSoftmax(x, base=1.5), label='Exp Softmax 1.5')
-    plt.plot(x, ReLU(x), label='ReLU')
+    plt.plot(x, ExpSoftmax(x, factor=1.0), label='Exp Softmax 1.0')
+    plt.plot(x, ExpSoftmax(x, factor=1.5), label='Exp Softmax 1.5')
+    plt.plot(x, Sigmoid(x), label='Sigmoid')
+    plt.plot(x, LeakyReLU(x, negative_slope=0.1), label='Leaky ReLU 0.1')
+    plt.plot(x, ReLU(x), label='ReLU') # dont sum to 1.
     plt.legend()
+    plt.xlabel('neural network output')
+    plt.ylabel('probability')
     # plt.savefig('normalization_functions.png')
     plt.show()
