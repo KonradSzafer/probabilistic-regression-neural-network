5a3fcb979426e6e4f7973dc9d0757d5ae93d379d
KonradSzafer <szafer.konrad@gmail.com>
2022-06-11 11:20:01 +0200

    renamed variables, assertion fix

--- src/urnn/urnn.py
+++ src/urnn/urnn.py
@@ -17,31 +17,31 @@ class URNN(nn.Module):
             min_value: float=0,
             max_value: float=1,
             latent_resolution: int=10,
-            plot_bin_precision: int=3
+            intervals_precision: int=3
         ):
         super(URNN, self).__init__()
 
         self.min_value = min_value
         self.max_value = max_value
         self.latent_res = latent_resolution
-        self.plot_bin_precision = plot_bin_precision
+        self.intervals_precision = intervals_precision
 
-        # bins for NN and ploting
-        self.bins = np.linspace(
+        # intervals for NN and ploting
+        self.intervals = np.linspace(
             self.min_value,
             self.max_value,
             num=self.latent_res+1
         )
-        self.bins_val_dict = {}
-        self.bins_str_dict = {}
-        for i in range(len(self.bins)-1):
-            self.bins_val_dict[i] = [
-                np.round(self.bins[i], self.plot_bin_precision),
-                np.round(self.bins[i+1], self.plot_bin_precision)
+        self.intervals_val_dict = {}
+        self.intervals_str_dict = {}
+        for i in range(len(self.intervals)-1):
+            self.intervals_val_dict[i] = [
+                np.round(self.intervals[i], self.intervals_precision),
+                np.round(self.intervals[i+1], self.intervals_precision)
             ]
-            self.bins_str_dict[i] = '<' + \
-                str(f'{self.bins[i]:.{self.plot_bin_precision}f}') + ', ' + \
-                str(f'{self.bins[i+1]:.{self.plot_bin_precision}f}') + ')'
+            self.intervals_str_dict[i] = '<' + \
+                str(f'{self.intervals[i]:.{self.intervals_precision}f}') + ', ' + \
+                str(f'{self.intervals[i+1]:.{self.intervals_precision}f}') + ')'
 
         # neural network
         self.model = nn.Sequential(
@@ -58,10 +58,10 @@ class URNN(nn.Module):
         return output
 
 
-    def print_bins(self) -> None:
-        print('class idx | bin min | bin max')
-        for i, bins in self.bins_str_dict.items():
-            print('%d:' % i, bins)
+    def print_intervals(self) -> None:
+        print('class idx | min | max')
+        for i, interval in self.intervals_str_dict.items():
+            print('%d:' % i, interval)
 
 
     def digitize(self, input: Tensor) -> Tensor:
@@ -73,7 +73,7 @@ class URNN(nn.Module):
         # digitalize
         output = np.digitize(
             input,
-            self.bins,
+            self.intervals,
             right=False
         )
         # classes starting from 0
@@ -91,8 +91,8 @@ class URNN(nn.Module):
         output = self.model(input.unsqueeze(0))
         _, label = torch.max(output, 1)
         label = label.item()
-        bin = self.bins_val_dict[label]
-        return output, label, bin
+        interval = self.intervals_val_dict[label]
+        return output, label, interval
 
 
     def plot_latent_distribution(self, output: Tensor, normalize: bool=True) -> None:
@@ -101,8 +101,8 @@ class URNN(nn.Module):
             output = nn.Softmax(dim=-1)(output)
         output = output.cpu().numpy()
         plt.bar(np.arange(len(output)), output)
-        plt.xticks(np.arange(len(output)), self.bins_str_dict.values(), rotation=45);
-        plt.xlabel('Bin index')
+        plt.xticks(np.arange(len(output)), self.intervals_str_dict.values(), rotation=45);
+        plt.xlabel('Interval index')
         plt.ylabel('Probability')
         plt.show()
 
@@ -111,10 +111,10 @@ class URNN(nn.Module):
 
         # check dimensions
         tensors_count, latent_size = input.shape
-        assert (
-            latent_size == self.latent_res,
-            f'incopatible latient size, expected {self.latent_res} got {latent_size}'
-        )
+        assert \
+            latent_size == self.latent_res, \
+            f'incopatible latient size, expected \
+            {self.latent_res} got {latent_size}'
 
         # output tensors
         value = torch.zeros([tensors_count, 1], dtype=torch.float16)
@@ -126,9 +126,9 @@ class URNN(nn.Module):
         for i in range(tensors_count):
             output: float = 0
             for j in range(latent_size):
-                bin_mid = mean(self.bins_val_dict[j])
-                bin_prob = input[i,j].item()
-                output += bin_mid * bin_prob
+                interval_mid = mean(self.intervals_val_dict[j])
+                interval_prob = input[i,j].item()
+                output += interval_mid * interval_prob
             value[i,0] = output
             probability[i,0], _ = torch.max(input[i], dim=-1)
         return value, probability
@@ -142,7 +142,7 @@ if __name__ == '__main__':
         max_value=1+1e-2, # must be little above the range of values
         latent_resolution=5
     )
-    model.print_bins()
+    model.print_intervals()
 
     target = torch.FloatTensor([0.1, 0.3, 0.5, 0.7, 0.9])
     target = model.digitize(target)
@@ -161,8 +161,8 @@ if __name__ == '__main__':
     print('Loss:', output)
 
     x = Tensor([0, 0.04, 0.07])
-    output, label, bin = model.predict_sample(x)
-    print(output, label, bin)
+    output, label, interval = model.predict_sample(x)
+    print(output, label, interval)
 
     value, probability = model.estimate(output)
     print('value:', value, 'probability:', probability)
